f4502a58a4ae4b0cdd2a1ffd778a0529cfbd14a4
Renato Richard <renatorichard2012@gmail.com>
2020-07-15 23:37:31 -0300

    playing with assert

---
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,25 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		Assert.assertEquals(1,1);
+		Assert.assertEquals(0.51234,0.51234, 0.01);
+		Assert.assertEquals(Math.PI, 3.14,0.01);
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i, i2.intValue());
+		
+		Assert.assertEquals("bola","bola");
+		Assert.assertEquals("bola", "Bola");
+		
+	}
+}
