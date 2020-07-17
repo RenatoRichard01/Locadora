d7bae7f66a30065208a828c3d0d843be7b9a3a8c
Renato Richard <renatorichard2012@gmail.com>
2020-07-16 23:57:26 -0300

    ]

--- src/test/java/br/ce/wcaquino/servicos/AssertTest.java
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -3,6 +3,8 @@ package br.ce.wcaquino.servicos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.ce.wcaquino.entidades.Usuario;
+
 public class AssertTest {
 	@Test
 	public void test() {
@@ -19,7 +21,24 @@ public class AssertTest {
 		Assert.assertEquals(i, i2.intValue());
 		
 		Assert.assertEquals("bola","bola");
-		Assert.assertEquals("bola", "Bola");
+		Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+		Assert.assertTrue("bola".startsWith("bola"));
+		
+		Usuario usr1 = new Usuario("usuario 1");
+		Usuario usr2 = new Usuario("usuario 1");
+		Usuario usr4 = usr2;
+		Usuario usr3 = null;
+		
+		Assert.assertEquals(usr1,usr2);
+		//verifica se objetos são de mesma instancia.
+		Assert.assertSame(usr2, usr4);
+		
+		//Verificar se valores são nulos
+		Assert.assertNull(usr3);
+		
+		//Utilizando do Not
+		Assert.assertNotSame(usr1,usr2);
+		Assert.assertNotNull(usr3);
 		
 	}
 }
